765597d27972ab39b28dfe6192fbb24c64e6285a
Enzo Vieira <enzovieira16@hotmail.com>
2022-02-03 15:53:51 +0000

    add(wall nodes): On clicking nodes, it turns into a wall node

--- src/Pathfinding/Node/index.tsx
+++ src/Pathfinding/Node/index.tsx
@@ -5,17 +5,24 @@ export interface INode {
   y: number;
   isStartNode: boolean;
   isFinishNode: boolean;
+  isWallNode: boolean;
 }
 
 interface IProps {
   node: INode;
+  onPressNode: (node: INode) => void;
 }
 
-export const Node = ({ node }: IProps) => {
+export const Node = ({ node, onPressNode }: IProps) => {
   const extraClassNode = node.isStartNode
     ? "start-node"
     : node.isFinishNode
     ? "finish-node"
     : "";
-  return <div className={`node ${extraClassNode}`}></div>;
+  return (
+    <td
+      className={`node ${extraClassNode} ${node.isWallNode ? "wall-node" : ""}`}
+      onClick={() => onPressNode(node)}
+    ></td>
+  );
 };
--- src/Pathfinding/index.tsx
+++ src/Pathfinding/index.tsx
@@ -1,37 +1,59 @@
+import { useEffect, useState } from "react";
 import { INode, Node } from "./Node";
 
 import "./Pathfinding.css";
 
-const nodes: INode[][] = [];
-for (let row = 0; row < 15; row++) {
-  const newRow: INode[] = [];
-
-  for (let column = 0; column < 50; column++) {
-    const newNode = {
-      x: column,
-      y: row,
-      isStartNode: row === 10 && column === 15,
-      isFinishNode: row === 10 && column === 40,
-    };
-
-    newRow.push(newNode);
-  }
-
-  nodes.push(newRow);
-}
-
-console.log(nodes);
-
 export const Pathfinding = () => {
+  const [nodes, setNodes] = useState<INode[][]>([]);
+
+  useEffect(() => {
+    const newNodes: INode[][] = [];
+    for (let row = 0; row < 15; row++) {
+      const newRow: INode[] = [];
+
+      for (let column = 0; column < 50; column++) {
+        const newNode = {
+          x: column,
+          y: row,
+          isStartNode: row === 10 && column === 15,
+          isFinishNode: row === 10 && column === 40,
+          isWallNode: false,
+        };
+
+        newRow.push(newNode);
+      }
+
+      newNodes.push(newRow);
+    }
+
+    setNodes(newNodes);
+  }, []);
+
+  const setWallNode = (node: INode) => {
+    const newNodes = [...nodes];
+    const currentNode = newNodes[node.y][node.x];
+    currentNode.isWallNode =
+      currentNode.isStartNode || currentNode.isFinishNode ? false : true;
+
+    setNodes(newNodes);
+  };
+
   return (
     <div>
       Pathfinding
       <table>
         <tbody>
-          {nodes.map((row) => (
-            <tr className="table-row">
+          {nodes.map((row, i) => (
+            <tr key={i} className="table-row">
               {row.map((node) => (
-                <Node {...{ node }} />
+                <Node
+                  key={`${node.x},${node.y}`}
+                  {...{ node }}
+                  onPressNode={(n) => {
+                    setWallNode(n);
+                    console.log(n);
+                  }}
+                />
               ))}
             </tr>
           ))}
